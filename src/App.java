7c3641608bcf56bada1273168c83cfbd71be485d
rafael <rafael.piedra@epn.edu.ec>
2023-06-13 22:13:33 -0500

    refactor: se agregó una exepción personalizada para poder controlar las entradas del sistema

--- src/App.java
+++ src/App.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Ingrese un numero entre 0 - 9999:  ");
-        int numero = input.nextInt();
-        if (numero >= 0 && numero <= 9999) {
+    public static void main(String[] args) throws NumeroFueraDeRango {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Ingrese un numero entre 0 - 9999:  ");
+            int numero = input.nextInt();
+            if (!(numero >= 0 && numero <= 9999)) {
+                throw new NumeroFueraDeRango("Número fuera de rango");
+            }
             System.out.println(traducirNumero(numero));
-        } else {
-            System.out.println("El numero ingresado esta fuera de rango.");
-        }
+
+
     }
 
     public static String traducirNumero(int numero) {
 
-        String[] unidades = { "", "shuk", "ishkay", "kimsa", "chusku", "pichka", "sukta", "kanchis", "pusak", "iskun" };
-        String[] decenas = { "", "chunka", "ishkay", "kimsa", "chusku", "pichka", "sukta", "kanchis", "pusak",
-                "iskun" };
+        String[] unidades = {"", "shuk", "ishkay", "kimsa", "chusku", "pichka", "sukta", "kanchis", "pusak", "iskun"};
+        String[] decenas = {"", "chunka", "ishkay", "kimsa", "chusku", "pichka", "sukta", "kanchis", "pusak",
+                "iskun"};
 
         int unidad = numero % 10;
         int decena = (numero / 10) % 10;
