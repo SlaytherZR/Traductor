3b2f5d87344f8aec8549ccafed16923e8e35c7e5
Rafael Piedra <rpiedra@cobiscorp.com>
2023-07-06 06:41:08 -0500

    feat:se implementaron las validaciones de formatod de dato

--- V2/Codigo/src/App.java
+++ V2/Codigo/src/App.java
@@ -1,25 +1,51 @@
-import java.util.Scanner;
-
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class App {
 
     public static void main(String[] args) throws NumeroFueraDeRango {
-
+        UIManager.put("OptionPane.okButtonText", "Traducir");
+        UIManager.put("OptionPane.cancelButtonText", "Salir");
         String dato = JOptionPane.showInputDialog(null, "Ingrese un número entre 1 - 9999 para traducirlo a Kichwa",
                 "Traductor Kichwa", JOptionPane.INFORMATION_MESSAGE);
-        int numero = Integer.parseInt(dato);
-        JOptionPane.showMessageDialog(null,
-                "El número " + numero + " traducido en Kichwa es:\n" + traducirNumero(numero), "Traductor Kichwa",
-                JOptionPane.INFORMATION_MESSAGE);
+        if (!esValido(dato)) {
+            UIManager.put("OptionPane.okButtonText", "ok");
+            JOptionPane.showMessageDialog(null, "El dato ingresado no cumple con los requisitos:\n- Debe ser un número entero positivo\n- Debe ser menor a 9999\n- No puede incluir el cero", "Error", JOptionPane.ERROR_MESSAGE);
+            int confirmResult = JOptionPane.showConfirmDialog(null, "¿Desea salir de la aplicación?", "Salir",
+                    JOptionPane.YES_NO_OPTION);
+
+            if (confirmResult == JOptionPane.YES_OPTION) {
+                System.exit(0);
+            } else {
+                main(args);
+            }
+        }
+
+        try {
+            int numero = Integer.parseInt(dato);
+            JOptionPane.showMessageDialog(null,
+                    "El número " + numero + " traducido en Kichwa es:\n" + traducirNumero(numero),
+                    "Traductor Kichwa", JOptionPane.INFORMATION_MESSAGE);
+            main(args);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "No se ha ingresado un número válido", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+            // Volver a mostrar el cuadro de diálogo de ingreso
+            main(args);
+        }
     }
+
+    private static boolean esValido(String dato) throws NumeroFueraDeRango {
+        return dato.matches("[1-9][0-9]{0,3}");
+    }
+
+
 
     // Metodo para traducir el numero
     public static String traducirNumero(int numero) {
 
-        String[] unidades = { "", "shuk", "ishkay", "kimsa", "chusku", "pichka", "sukta", "kanchis", "pusak", "iskun" };
-        String[] decenas = { "", "chunka", "ishkay", "kimsa", "chusku", "pichka", "sukta", "kanchis", "pusak",
-                "iskun" };
+        String[] unidades = {"", "shuk", "ishkay", "kimsa", "chusku", "pichka", "sukta", "kanchis", "pusak", "iskun"};
+        String[] decenas = {"", "chunka", "ishkay", "kimsa", "chusku", "pichka", "sukta", "kanchis", "pusak",
+                "iskun"};
         // Variables para obtener los valores de cada numero
         int unidad = numero % 10;
         int decena = (numero / 10) % 10;
